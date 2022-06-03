37f117aa19190a9a8dc7519c39f433cb091037b8
Thavhat <tsiwant@telkom.co.za>
2022-06-03 08:12:14 +0200

    redo

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
     elif len(sys.argv) == 2:
         print("1 argument:\n1: {}".format(sys.argv[1]))
     else:
-        length = len(sys.argv) 
+        length = len(sys.argv)
         print("{} arguments:".format(length-1))
-        for i in range(2,length+1):
-            print("{}: {}".format(i-1,sys.argv[i-1]))
+        for i in range(2, length+1):
+            print("{}: {}".format(i-1, sys.argv[i-1]))
+            
