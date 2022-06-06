7e12c56318247246f345c6ccd21f7d9a661235dc
Thavhat <tsiwant@telkom.co.za>
2022-06-06 08:05:27 +0200

    task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    n_list = my_list
+    if idx < 0 or idx > len(my_list):
+        return my_list
+    else:
+        n_list[idx] = element
+        return n_list
