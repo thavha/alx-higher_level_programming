7a08348d9ef73110ff649e89ec37c35a90edc107
Thavhat <tsiwant@telkom.co.za>
2022-06-06 09:54:03 +0200

    task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(0,0), tuple_b=(0,0)):
+    if len(tuple_a) == 1:
+        tuple_a = tuple((tuple_a[0],0))
+    if len(tuple_b) == 1:
+        tuple_b = tuple((tuple_b[0],0))
+    if len(tuple_a) == 0:
+        tuple_a = tuple((0,0))
+    if len(tuple_b) == 0:
+        tuple_b = tuple((0,0))
+    return tuple((tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]))
