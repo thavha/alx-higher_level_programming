e48fe7c0a12b2a1e120b74a0c2834b14ac67db93
Thavhat <tsiwant@telkom.co.za>
2022-06-07 07:28:35 +0200

    redo 9

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     length = len(sentence)
     if length == 0:
-        return tuple((0,None))
+        return tuple((0, None))
     else:
-        return tuple((length,sentence[0]))
+        return tuple((length, sentence[0]))
