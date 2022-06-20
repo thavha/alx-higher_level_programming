cdc6208a185ce504cf880a609b2f47f1db4549cf
Thavhat <tsiwant@telkom.co.za>
2022-06-20 15:03:22 +0200

    task 2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
+def safe_print_list(my_list=[], x = 0):
     a = 0
     i = 0
     while a < x:
         try:
-            print(my_list[a], end="")
+            print(my_list[a], end = "")
             i = i + 1
         except:
             pass
